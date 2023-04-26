27967369aed41e507d3a6fb75822e8989564da44
seah78 <webmaster@seah.dev>
2023-04-26 05:31:28 +0200

    add login endpoint and token decorator

--- api.py
+++ api.py
@@ -1,6 +1,9 @@
-from flask import Flask, request, jsonify
+import datetime
+from flask import Flask, request, jsonify, make_response
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
+import jwt
+from functools import wraps
 
 from config import Config
 from models import db, User, Ingredient, init_app
@@ -9,6 +12,30 @@ app = Flask(__name__)
 app.config.from_object(Config)
 
 init_app(app)
+
+# decorator
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({'message' : 'Token is missing!'}), 401
+
+        try: 
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = User.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message' : 'Token is invalid!'}), 401
+
+        return f(current_user, *args, **kwargs)
+
+    return decorated
+
+
 
 # ENDPONTS
 
@@ -21,7 +48,12 @@ def home():
 
 # Show all users
 @app.route('/user', methods=['GET'])
-def get_all_users():
+@token_required
+def get_all_users(current_user):
+    
+    if not current_user.admin:
+        return jsonify({'message' : 'Cannot perform that function!'})
+    
     users = User.query.all()
 
     output = []
@@ -38,12 +70,13 @@ def get_all_users():
 
 # Show one user
 @app.route('/user/<public_id>', methods=['GET'])
-def get_one_user(public_id):
+@token_required
+def get_one_user(current_user, public_id):
     
     user = User.query.filter_by(public_id=public_id).first()
     
     if not user:
-        return jsonify({'message' : 'user not found'})
+        return jsonify({'message' : 'User not found'})
     
     user_data = {}
     user_data['public_id'] = user.public_id
@@ -55,7 +88,8 @@ def get_one_user(public_id):
 
 # Create user
 @app.route('/user', methods=['POST'])
-def create_user():
+@token_required
+def create_user(current_user):
     data = request.get_json()
     
     hashed_password = generate_password_hash(data['password'], method='sha256')
@@ -68,20 +102,22 @@ def create_user():
 
 # Update user
 @app.route('/user/<public_id>', methods=['PUT'])
-def update_user(public_id):
+@token_required
+def update_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
     
     if not user:
-        return jsonify({'message' : 'user not found'})
+        return jsonify({'message' : 'User not found'})
 
     user.is_admin = True
     db.session.commit()
 
-    return jsonify({'message' : 'The user has been promoted!'})
+    return jsonify({'message' : 'The user has been updated!'})
 
 # Delete user
 @app.route('/user/<public_id>', methods=['DELETE'])
-def delete_user(public_id):
+@token_required
+def delete_user(current_user, public_id):
     user = User.query.filter_by(public_id=public_id).first()
 
     if not user:
@@ -91,3 +127,23 @@ def delete_user(public_id):
     db.session.commit()
 
     return jsonify({'message' : 'The user has been deleted!'})
+
+# Login
+@app.route('/login')
+def login():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    user = User.query.filter_by(username=auth.username).first()
+
+    if not user:
+        return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
+
+    if check_password_hash(user.password, auth.password):
+        token = jwt.encode({'public_id' : user.public_id, 'exp' : datetime.datetime.utcnow() + datetime.timedelta(minutes=30)}, app.config['SECRET_KEY'])
+
+        return jsonify({'token' : token.decode('UTF-8')})
+
+    return make_response('Could not verify', 401, {'WWW-Authenticate' : 'Basic realm="Login required!"'})
